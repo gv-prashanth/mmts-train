6f6bdc81d7cb05ecae03aa21597ad91a7db56ad6
gv-prashanth <gvprashanth.android@gmail.com>
2018-04-02 19:57:21 +0530

    having other elements
    having other elements

--- src/main/java/com/vadrin/mmtstrain/google/dto/GoogleResponse.java
+++ src/main/java/com/vadrin/mmtstrain/google/dto/GoogleResponse.java
@@ -1,10 +1,48 @@
 package com.vadrin.mmtstrain.google.dto;
 
+import java.util.List;
+
 public class GoogleResponse {
 
 	private String speech;
 	private String displayText;
 	private Messages messages;
+	private Object data;
+	private List<Object> contextOut;
+	private String source;
+	private Object followupEvent;
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+	public List<Object> getContextOut() {
+		return contextOut;
+	}
+
+	public void setContextOut(List<Object> contextOut) {
+		this.contextOut = contextOut;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public Object getFollowupEvent() {
+		return followupEvent;
+	}
+
+	public void setFollowupEvent(Object followupEvent) {
+		this.followupEvent = followupEvent;
+	}
 
 	public String getSpeech() {
 		return speech;
