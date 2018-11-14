d7c049e04fe6dd95e13ff1a1f30a9c75423fa0df
Prashanth Venkat Gumma <pragumma0@publicisgroupe.net>
2018-11-14 16:49:05 +0530

    try with session id

--- src/main/java/com/vadrin/mmtstrain/controllers/GoogleHomeCallbackController.java
+++ src/main/java/com/vadrin/mmtstrain/controllers/GoogleHomeCallbackController.java
@@ -23,13 +23,9 @@ public class GoogleHomeCallbackController {
 	@RequestMapping(value = { "/callback/google" }, method = { RequestMethod.POST })
 	public GoogleResponse getChat(@RequestBody JsonNode requestBody) {
 		System.out.println("request is - " + requestBody.toString());
-		if (requestBody.get("result").get("metadata").has("intentName")) {
-			Chat input = new Chat(requestBody.get("result").get("parameters").get("userInput").asText());
-			Chat output = mmtsTrainController.converse("123", input);
-			return googleHomeService.constructGoogleResponse(output);
-		}else {
-			return googleHomeService.constructGoogleResponse("NEEDS DEVELOPMENT", false);
-		}
+		Chat input = new Chat(requestBody.get("result").get("parameters").get("userInput").asText());
+		Chat output = mmtsTrainController.converse(requestBody.get("sessionId").asText(), input);
+		return googleHomeService.constructGoogleResponse(output);
 	}
 
 }
