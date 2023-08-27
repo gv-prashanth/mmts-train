7aadee9fd304e418608e088a714e4952540cc0d1
Gumma <3628612@lowes.com>
2023-08-27 16:25:40 +0530

    code revert

--- src/main/java/com/vadrin/mmtstrain/controllers/AlexaCallbackController.java
+++ src/main/java/com/vadrin/mmtstrain/controllers/AlexaCallbackController.java
@@ -1,20 +1,15 @@
 package com.vadrin.mmtstrain.controllers;
 
-import java.io.IOException;
-import java.util.stream.Collectors;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.vadrin.mmtstrain.models.alexa.AlexaResponse;
 import com.vadrin.mmtstrain.services.AlexaService;
-import com.vadrin.mmtstrain.services.JsonService;
 
 @RestController
 public class AlexaCallbackController {
@@ -22,16 +17,11 @@ public class AlexaCallbackController {
 	@Autowired
 	AlexaService alexaService;
 
-	@PostMapping("/callback/alexa")
-	public ResponseEntity<AlexaResponse> callback(HttpServletRequest request) {
-		try {
-	    JsonNode alexaRequestBody = JsonService.getJson(request.getReader().lines().collect(Collectors.joining(System.lineSeparator())));
-	    System.out.println("request is - " + alexaRequestBody.toString());
-	    return new ResponseEntity<>(alexaService.respond(alexaRequestBody), HttpStatus.OK);
-		} catch (IOException e) {
-      e.printStackTrace();
-      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-	}
+  @PostMapping("/callback/alexa")
+  public ResponseEntity<AlexaResponse> callback(@RequestBody JsonNode alexaRequestBody) {
+    // JsonNode alexaRequestBody = JsonService.getJson(request.getReader().lines().collect(Collectors.joining(System.lineSeparator())));
+    System.out.println("request is - " + alexaRequestBody.toString());
+    return new ResponseEntity<>(alexaService.respond(alexaRequestBody), HttpStatus.OK);
+  }
 
 }
